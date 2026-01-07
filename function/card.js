83e309757360db5fb284b42c835055841093f255
loic-merlhe <151088440+lazzy3457@users.noreply.github.com>
2026-01-07 12:41:44 +0100

    update 14 Card

--- function/card.js
+++ function/card.js
@@ -3,6 +3,11 @@
 const liste = {};
 
 export function Card (conteneur, titre, contenue, progressBar) {
+    // le conteneur : est l'element html qui va receuillir la card
+    // le titre : est le titre de la card
+    // le contenue : est le cours et les question au format JSON
+    // {cours : "<h2>je suis du cours</h2><p>explication du cours</p>",
+    //   quizz : [{question : "question 1", reponses : ["reponse 1", "reponse 2", "reponse 3"], type : "radio", valide : ["index_bonne_reponse"]}, {question : "question 1", reponses : ["reponse 1", "reponse 2", "reponse 3"], type : "checkbox", valide : ["index_bonne_reponse1", "index_bonne_reponse2"]}]}
 
     let div = document.getElementById(conteneur);
 
